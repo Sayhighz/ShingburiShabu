e1b5a6741e711d3dc52a8bacebe48cff1bac2735
dawinzhy99 <sutha.tho@spumail.net>
2024-04-22 16:35:50 +0700

    <BR> Name and price

--- client/src/components/Ordering/ItemMenu.jsx
+++ client/src/components/Ordering/ItemMenu.jsx
@@ -22,7 +22,8 @@ function ItemMenu(props) {
                         <img src={`http://localhost:3000/Images/`+image} alt="" className='menuimage h-60 w-60 mt-5'/>
                     </figure>
                     <div className="card-body">
-                        <h2 className="card-title">Name : {name} Price : {price}</h2>
+                        <h2 className="card-title">Name : {name} </h2>
+                        <h2>Price : {price}</h2>
                         <div className='flex justify-center'>
                             <button disabled={showBtn} onClick={orderItem} className='text-xl border border-inherit rounded-md p-1 bg-white text-black w-28'>Order</button>
                         </div>
